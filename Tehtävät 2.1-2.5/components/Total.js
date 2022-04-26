63854c70c8b4f60cb257ec10cc6bf53683a20470
Shapetaker <79154928+Shapetaker@users.noreply.github.com>
2022-04-26 13:35:28 +0300

    Add files via upload

---
+++ Tehtävät 2.1-2.5/components/Total.js
@@ -0,0 +1,9 @@
+
+const Total = (props) => {
+    return (
+      <p><b>total of {props.parts.reduce( (sum, part)=> sum + part.exercises, 0)} exercises </b></p>
+    )
+  }
+  
+  export default Total
+    
